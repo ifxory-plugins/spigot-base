d3640699e3442a3e5141658a9926978f59329b63
ifxory-plugins <63095272+ifxory-plugins@users.noreply.github.com>
2020-09-02 21:14:19 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 echo "Cloning repo..."
-git clone --single-branch --branch ${1} https://github.com/ifxory-plugins/spigot-base
-read -p "You want to install a customization README file for yourself?" readme
-[[ $readme != [yY] ]] && exit 1
-git clone --single-branch --branch readme https://github.com/ifxory-plugins/spigot-base
+git clone --single-branch --branch ${1} https://github.com/ifxory-plugins/spigo>
+mv spigot-base/* .
+rm spigot-base -rf
