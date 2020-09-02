09cee62449f728205c46b44dfbf889cf703c2cf4
ifxory-plugins <63095272+ifxory-plugins@users.noreply.github.com>
2020-09-02 20:53:40 +0300

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-echo "Cloning repo..."
-git clone --single-branch --branch ${1} https://github.com/ifxory-plugins/spigot-base
-read -p "You want to install a customization README file for yourself?" readme
-[[ $readme != [yY] ]] && exit 1
-git clone --single-branch --branch readme https://github.com/ifxory-plugins/spigot-base
